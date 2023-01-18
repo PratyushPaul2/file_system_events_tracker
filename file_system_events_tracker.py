69551c1c55ba0b922072a0b28ff2ae2d3a0f05dc
PratyushPaul2 <74598606+PratyushPaul2@users.noreply.github.com>
2023-01-18 21:31:02 +0530

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,53 @@
+import sys
+import time
+import random
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from_dir="C:/Users/prita/OneDrive/Desktop/Source/test1.docx"
+to_dir="C:/Users/prita/OneDrive/Desktop/Destination"
+dir_tree={
+    "image_files":['.jpg','.png','.jpeg','.gif','.jfif'],
+    "video_files":['.mpg','.mp2','.mpeg','.mpv','.mpe','.m4p','.m4v','.avi','.mov'],
+    "document_files":['.ppt','.xls','.docx','.xlsx','.csv','.pdf','.txt'],
+    "setup_files":['.exe','.bin','.cmd','.msi','.dmg'],
+}
+
+class FileMovementHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        name,extension=os.path.splitext(event.src_path)
+        time.sleep(1)
+        for key,value in dir_tree.items():
+            time.sleep(1)
+            if extension in value:
+                file_name=os.path.basename(event.src_path)
+                print("Downloaded "+file_name)
+                path1=from_dir+"/"+file_name
+                path2=to_dir+"/"+key
+                path3=to_dir+"/"+key+"/"+file_name
+                if os.path.exists(path2):
+                    print("Directory Exists")
+                    print("Moving "+file_name+"...")
+                    shutil.move(path1,path3)
+                    time.sleep(1)
+                else:
+                    print("Making Directory")
+                    os.makedirs(path2)
+                    print("Moving "+file_name+"...")
+                    shutil.move(path1,path3)
+                    time.sleep(1)
+
+event_handler=FileMovementHandler()
+observer=Observer()
+observer.schedule(event_handler,from_dir,recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("Running")
+except KeyboardInterrupt:
+    print("Stopped")
+    observer.stop()
